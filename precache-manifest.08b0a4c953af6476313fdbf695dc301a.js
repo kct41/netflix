4a9579561a2a86625d8b3e04615aea3b0fdfbe37
somi kim <som2mos@naver.com>
2022-04-11 15:02:46 +0900

    Updates

---
+++ precache-manifest.08b0a4c953af6476313fdbf695dc301a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c03584466d74040bb2ed",
+    "url": "/netflix/static/css/main.ec35f5b6.chunk.css"
+  },
+  {
+    "revision": "c03584466d74040bb2ed",
+    "url": "/netflix/static/js/main.c0358446.chunk.js"
+  },
+  {
+    "revision": "aa1ecd07d8b85afa671e",
+    "url": "/netflix/static/js/1.aa1ecd07.chunk.js"
+  },
+  {
+    "revision": "86999975e4a9bc9c6f5d",
+    "url": "/netflix/static/js/2.86999975.chunk.js"
+  },
+  {
+    "revision": "8d5dd2de70acf5fc548f",
+    "url": "/netflix/static/js/runtime~main.8d5dd2de.js"
+  },
+  {
+    "revision": "2ea85d7448475a744c1485c2eac3d3d1",
+    "url": "/netflix/static/media/netflix-logo-0.2ea85d74.png"
+  },
+  {
+    "revision": "5fadd83a0e501971f9a5df6dac89a452",
+    "url": "/netflix/index.html"
+  }
+];
